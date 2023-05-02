0d981c95a83e5ba2b8e43f09d0b0c5672c213192
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-05-02 13:19:52 -0700

    adding conversation model

---
+++ utils/server/models/ConversationModel.ts
@@ -0,0 +1,17 @@
+import { Pool, Client } from 'pg';
+
+const myURI = process.env.URI;
+
+const pool = new Pool({
+  connectionString: myURI,
+  port: 5432,
+});
+
+const db = {
+  query: (text: string, params: any, callback: any) => {
+    console.log('executed query: ', text)
+    return pool.query(text, params, callback)
+  }
+};
+
+export default db;
