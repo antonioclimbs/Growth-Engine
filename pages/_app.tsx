098ef703a7a28c885fcc39c91d2004d38f94936b
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2023-05-05 12:56:02 -0700

    fixing google inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,23 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
+import { GoogleFonts } from 'next-google-fonts';
+
 
 import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
   const queryClient = new QueryClient();
 
   return (
-    <div className={inter.className}>
+    <div>
+      {/* <div className={inter.className}> */}
       <Toaster />
       <QueryClientProvider client={queryClient}>
+        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
         <Component {...pageProps} />
       </QueryClientProvider>
     </div>
